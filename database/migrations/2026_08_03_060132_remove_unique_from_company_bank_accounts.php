e4869f49d1fcf295aca7d180441733adc99643ed
hasinthafeeder-coder <hasinthafeeder@gmail.com>
2026-08-11 18:35:02 +0530

    002-migration-change-for-vps

--- database/migrations/2026_08_03_060132_remove_unique_from_company_bank_accounts.php
+++ database/migrations/2026_08_03_060132_remove_unique_from_company_bank_accounts.php
@@ -9,18 +9,34 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('company_bank_accounts', function (Blueprint $table) {
+            $table->dropForeign('company_bank_accounts_company_id_foreign');
             $table->dropUnique('company_bank_accounts_company_id_unique');
-
             $table->index('company_id');
+        });
+
+        Schema::table('company_bank_accounts', function (Blueprint $table) {
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
         });
     }
 
     public function down(): void
     {
         Schema::table('company_bank_accounts', function (Blueprint $table) {
+            $table->dropForeign('company_bank_accounts_company_id_foreign');
             $table->dropIndex('company_bank_accounts_company_id_index');
-
             $table->unique('company_id');
+        });
+
+        Schema::table('company_bank_accounts', function (Blueprint $table) {
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
         });
     }
 };
